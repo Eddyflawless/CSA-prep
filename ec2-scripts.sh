806ab1edf14755eb52f288b193070532bef80424
Edwin Djonorh <edjonorh@gmail.com>
2022-03-14 10:01:10 +0000

    added more functions

--- ec2-scripts.sh
+++ ec2-scripts.sh
@@ -1,5 +1,76 @@
 #!/bin/bash
 
-hibernate(){
-    
+config(){
+    #Note:  the  values  you  provide  for the AWS Access Key ID and the AWS
+    #Secret Access Key will  be  written  to  the  shared  credentials  file
+    #(~/.aws/credentials).
+    aws configure set aws_access_key_id $1
+    aws configure set aws_secret_access_key $2
+    aws configure set default.region $3
+
 }
+
+get_aws_configuration(){
+
+    config=""
+    command="aws configure get"
+
+    case "$1" in
+        "aws_access_key_id")
+        config=$($command aws_access_key_id) 
+        ;;
+        "aws_secret_access_key")
+        config=$($command aws_secret_access_key) 
+        ;;
+        "region")
+        config=$($command region) 
+        ;;
+        *)
+        exit
+        ;;
+    esac
+
+    echo $config
+
+}
+
+list_profiles(){
+    aws configure list
+}
+
+hibernate(){
+
+    if [[  -z "$1" ]];then
+        printf "Provide an instance-id to start-up eg. i-0d91a3xxxxxxxx"
+        printf "Actual usage: $0 start_instance i-0d91a3xxxxxxx"
+        exit
+    fi
+
+    aws ec2 stop-instances \
+    --instance-ids $1 \
+    --hibernate 
+
+}
+
+start_instances(){
+
+    if [[  -z "$1" ]];then
+        printf "Provide an instance-id to start-up eg. i-0d91a3xxxxxxxx"
+        printf "Actual usage: $0 start_instance i-0d91a3c43c8e88855"
+        exit
+    fi
+
+    aws ec2 start-instances  \
+    --instance-ids $1
+}
+
+command=""
+
+for x in "$@"; do
+    command=$command" $x"
+done
+
+echo ""
+printf "command is $command \n"
+
+eval "$command" 
