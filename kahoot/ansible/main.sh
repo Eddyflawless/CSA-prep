eacdd5a29ee4bc1e3bc5f6f956cd9ed4d0c17048
Edwin Djonorh <edjonorh@gmail.com>
2022-03-18 16:15:00 +0000

    added new utility helpers like creating a playbook, logger and automatic suite execution

--- kahoot/ansible/main.sh
+++ kahoot/ansible/main.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-sudo su
-source ./constants.sh
+#sudo su
+
+source ./constants.sh #very important inclustion
+
+time=$(date '+%H:%M:%S')
+output_file="reports/report-$(date '+%Y-%m-%d').txt"
+
+function print_line(){
+    echo -e "------------------------------------------------- \n"
+}
+
+function log(){
+
+    #log into console
+    log_output=$1
+    echo $log_output
+
+    print_line
+
+    #log into file
+    echo -e "$1" >> $output_file
+
+}
 
 
 function manual(){
@@ -51,33 +72,146 @@ function reportMsg(){
     "
 }
 
+function execute_command(){
+
+    arguments=""
+
+    for t in $@; do
+        arguments="$arguments $t"
+    done
+
+    echo "execute ==> $arguments"
+
+    
+}
+
+function ping_host(){
+
+    arguments=$(echo "$1" | sed 's/,/ /g')
+
+    for arg in ${arguments[@]}; do 
+        eval "ping -t 2 ${arg}"
+    done
+    
+}
+
+
+function create_new_playbook(){
+
+    play_book_template="""
+#!/bin/bash
+
+echo '$0 executing script play....'
+
+pem_file='pems/sample.pem'
+
+if [[ -f $pem_file ]]; then
+
+    exit
+
+fi
+
+#update os
+
+#update specific security patch for software(s)
+
+#reload a service(s)
+
+    """
+    host_template="""
+#eg. 45.56.72.153
+    """
+    
+    count=$( expr $(ls -l $PLAY_DIR | wc -l)  + 1 )
+
+    new_dir="$PLAY_DIR/play.$count"
+
+    #create folder
+    echo "Creating parent folder...."
+    mkdir -p "$new_dir"
+
+    echo "Creating necessary files..."
+    #add files
+    echo "$play_book_template" >> "$new_dir/play.sh"
+    echo "$host_template" >> "$new_dir/host.cfg"
+
+    #change permissions
+    chmod +x "$new_dir/play.sh"
+
+}
+
+function run_play_suite(){
+
+    for play_folder in $(ls ./plays); do 
+
+        current_folder="plays/$play_folder"
+        
+        play_book="$current_folder/play.sh"
+
+        if [[ -f  $play_book ]]; then
+
+            if [[ -x $play_book ]]; then
+                chmod +x $play_book
+            fi
+
+            echo "executing $play_book"
+
+            log "[playbook:$play_book $time]: Executing...."
+            
+            eval "$play_book >> $output_file"
+
+            log "[playbook:$play_book $time]: Done! \n"
+
+        fi
+
+    done
+
+}
+
 function preCheck(){
 
     #check for bin commands
 
-    command -V ssh || (
-
+    command -v ssh || (
+        warn "Ensure ssh is installed on your linux distributtion"
     )
 
-    command -V curl || (
-
+    command -v curl || (
+        warn "Ensure ssh is installed on your linux distributtion"
     )
 
     #check for constants
 }
 
 
+command_str=unset
 
-while getopts "a:m:" option;do 
+while getopts "m:c:ba" option;do 
 
     case ${option} in 
 
         a) 
-            echo "Execute this command $OPTARG instead "
+            echo "Execute play suite.... "
+            command_str="run_play_suite"
             shift
         ;;
-        -h)
+        m)
             echo "Excute module name $OPTARG"
+            command_str="ping_host $OPTARG"
+            shift
+        ;;
+        c)
+            echo "Excute module name $OPTARG"
+            command_str="execute_command $OPTARG"
+        ;;
+        p)
+            echo "Excute module name $OPTARG"
+            command_str=""
+            shift
+        ;;
+        b)
+            echo "Create new playbook $OPTARG"
+            command_str="create_new_playbook"
             shift
         ;;
         \?)
@@ -88,4 +222,12 @@ while getopts "a:m:" option;do
 
 
 done
+
+preCheck
+
+if [[ ! -z "$command_str" ]]; then 
+    eval "$command_str"
+fi
+
+#run_play_suite
 
